f0e0bc84f55d3dcb99f30ab6637d36a431ab3011
Johan809 <20186764@itla.edu.do>
2020-11-13 17:57:16 -0400

    home n' error404 components

--- itla-med/src/app/components/home/home.component.ts
+++ itla-med/src/app/components/home/home.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  public title: string;
+  public description: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.title = 'Bienvenido a ITLA Med!';
+    this.description =
+      'Esta es una app web con presencia móvil que facilita a los doctores el manejo de la información correspondiente a sus pacientes y consultas; además de brindar distintos tipos de reportes, haciendo mas sencilla la gestion de información.';
   }
-
 }
