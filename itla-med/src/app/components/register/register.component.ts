c1f84976ad8d1cb0aebc6d156adfa4a10d7640ca
rickylasalsa <monteroricky4@gmail.com>
2020-11-18 16:34:20 -0400

    Ricky

--- itla-med/src/app/components/register/register.component.ts
+++ itla-med/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { promise } from 'protractor';
 import { Doctor } from '../models/doctor.model';
 import { ServerService } from "../server.service";
@@ -10,7 +11,8 @@ import { ServerService } from "../server.service";
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private servi: ServerService) { }
+  constructor(private servi: ServerService,
+    private router: Router,) { }
 
   public user:Doctor = {
     name: "",
@@ -18,16 +20,26 @@ export class RegisterComponent implements OnInit {
     password: ""
   }
 
- public async register(user:any): Promise<any>{
-   const d = new user();
+ public async registrar(name,email,clave)
 
-   return this.servi.register(user);
+ {
+
+  this.user.name = name,
+  this.user.email = email,
+  this.user.password = clave
+
+
+  if (this.user.name === null || this.user.name ==='') {
+   alert("Este campo es obligatorio")
+  }
+  else{
+    this.servi.register(this.user)
+  }
+
  }
-
-
-
-
+ 
   ngOnInit(): void {
+    this.registrar
   }
 
 }
