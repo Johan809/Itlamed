a35a81a5b724aa997a76c69b0516585db2c1470d
raulgalarza <74329767+raulgalarza@users.noreply.github.com>
2020-11-21 00:55:19 -0400

    Update login.component.ts

--- itla-med/src/app/components/login/login.component.ts
+++ itla-med/src/app/components/login/login.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserInfo } from 'os';
+import { promise, logging } from 'protractor';
+import { ServerService } from '../server.service';
+import { Doctor } from '../../models/doctor.model';
+import { Consult } from '../../models/consult.model';
+import { LobbyComponent } from '../lobby/lobby.component';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +15,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private servi: ServerService,
+    private router: Router) { }
+
+  public user: Doctor = {
+    email: '',
+    password: '',
+    name: '',
+  };
+
+  public async login(email:string,clave:string)
+
+  {
+ 
+   if (this.user.email == 'raul' && this.user.password =='1234') {
+     alert('probando enrutamiento')
+   }
+   
+   else{
+     this.servi.login(this.user)
+   }
+ 
+  }
 
   ngOnInit(): void {
-  }
+
+    this.login
+  } 
 
 }
